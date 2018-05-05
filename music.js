a9b77d93e897f866ab78b3d507e1f34881de487d
regouga <miguelregouga@tecnico.ulisboa.pt>
2018-05-05 12:15:24 +0100

    Fixed HTTPS issues and body updated

--- music.js
+++ music.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $.getJSON("http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=regouga&api_key=0486b81ad30a89efc0ad41b0b65dae95&limit=2&format=json&callback=?", function(data) {
+    $.getJSON("https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=regouga&api_key=0486b81ad30a89efc0ad41b0b65dae95&limit=2&format=json&callback=?", function(data) {
 
         var html = ''; 
         var counter = 1; 
